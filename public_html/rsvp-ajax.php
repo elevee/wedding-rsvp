82379ae2915d7f71862bdc31224698660674ae43
Eric Levine <djlevine@gmail.com>
2018-01-07 20:44:05 -0800

    Added test file, some code from elsewhere

--- public_html/rsvp-ajax.php
+++ public_html/rsvp-ajax.php
@@ -1,11 +1,14 @@
 <?php
 date_default_timezone_set('America/Los_Angeles');
+require_once __DIR__ . '/../rsvp-config.php';
+
 $allowed = $ALLOWED_ORIGINS;
 if(isset($_SERVER['HTTP_ORIGIN'])){
-	in_array($_SERVER['HTTP_ORIGIN'], $allowed);
+	in_array($_SERVER['HTTP_ORIGIN'], $allowed); //this isn't doing anything rn...
 	header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
 	header("Access-Control-Allow-Methods: GET, POST");	
 }
+// include_once(__DIR__ . '/../rsvp/rsvp.php');
 // print_r($_SERVER);
 
 if(!isset($_SERVER["SERVER_NAME"])){ //if running in cli for testing...
@@ -17,6 +20,8 @@ $method = isset($_SERVER['REQUEST_METHOD']) && is_string($_SERVER['REQUEST_METHO
 // echo("Server request method is ". $_SERVER['REQUEST_METHOD']."\n");
 // print_r($_GET);
 
+
+
 if($method === "GET"){
 	// echo("We inside the get\n");
 	if($_GET["type"] !== null && $_GET["type"] === "lookup"){
@@ -24,7 +29,7 @@ if($method === "GET"){
 		if(!is_null($_GET["query"]) && is_string($_GET["query"])){
 			$cmd = "php -f ../rsvp/rsvp.php ".$method." ".trim($_GET["query"]);
 			// echo("Calling ".$cmd."\n");
-			echo(exec($cmd)); 
+			echo(exec($cmd));
 		}
 	} else {
 		echo("shiz failed, cuz");
@@ -32,14 +37,13 @@ if($method === "GET"){
 }
 
 if($method === "POST"){
-	echo("Post type is ". $_POST["type"]);
 	if($_POST["type"] !== null && $_POST["type"] === "confirm"){
-		echo("We inside the type");
+		// echo("We inside the type");	
+		// print_r($_POST);
 		if(!is_null($_POST["inviteCode"]) && is_string($_POST["inviteCode"])){
-			$cmd = "php -f ../rsvp/rsvp.php ".$method." ".trim($_POST["inviteCode"])." ".$_POST["decision"].$_POST["notes"];
+			$cmd = "php -f ../rsvp/rsvp.php ".$method." ".trim($_POST["inviteCode"])." ".$_POST["attending"]." ".$_POST["num_attending"]." \"".addslashes($_POST["notes"])."\"";
 			
-			echo("Calling ".$cmd."\n");
-			exit();
+			// echo("Calling ".$cmd."\n");
 			echo(exec($cmd)); 
 		}
 	} else {
--- rsvp/rsvp.php
+++ rsvp/rsvp.php
@@ -3,7 +3,7 @@ date_default_timezone_set('America/Los_Angeles');
 header('Content-type:application/json;charset=utf-8');
 
 require_once __DIR__ . '/vendor/autoload.php';
-// require_once __DIR__ . '/../rsvp-config.php';
+require_once __DIR__ . '/rsvp-config.php';
 
 define('APPLICATION_NAME', 'Wedding RSVP');
 define('CREDENTIALS_PATH', '~/.credentials/weddingRSVP.json');
@@ -75,89 +75,150 @@ function expandHomeDirectory($path) {
 // ------------------------------------------------
 
 // parse_str(implode('&', array_slice($argv, 1)), $_GET);
-echo("GET VARS:\n");
-print_r($_GET);
+// echo("GET VARS:\n");
+// print_r($_GET);
 // Get the API client and construct the service object.
 // $client = getClient();
+
 $client = new Google_Client();
 $client->setApplicationName(APPLICATION_NAME);
 $client->setScopes(SCOPES);
-$api_key = "";
+$api_key = $API_KEY;
 $client->setDeveloperKey($api_key);
 $client->setAccessType("offline");
-
 $service = new Google_Service_Sheets($client);
-$method = isset($_SERVER['REQUEST_METHOD']) && is_string($_SERVER['REQUEST_METHOD']) && in_array($_SERVER['REQUEST_METHOD'], array("GET", "POST")) ? $_SERVER['REQUEST_METHOD'] : "GET";
-//isset($argv[1]) && is_string($argv[1]) && strlen($argv[1]) > 0 ? $argv[1] : null;
 
 // Get the values from the spreadsheet
-$spreadsheetId = "";
-$range = 'Guestlist!A:M';
+$spreadsheetId = $SPREADSHEET_ID;
+$range = 'Guestlist!A:O';
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
 $values = $response->getValues();
-// exit();
-// $inviteCode = $_POST['code'];
-$inviteCode = (isset($argv[2]) && is_string($argv[2]) && strlen($argv[2]) > 0) ? $argv[2] : null;
-if($method === "POST"){
-	$attending 			= "N";//(isset($_POST["attending"]) && is_bool($_POST["attending"]) ? $_POST["attending"] : null);
-	$number_attending 	= 50;//(isset($_POST["size"]) && strlen(trim($_POST["size"])) > 0 ? trim($_POST["size"]) : null);
+// if (count($values) == 0) {
+//   print "No data found.\n";
+// } else {
+//   print "Name, Party:\n";
+//   foreach ($values as $row) {
+//     // Print columns A and B, which correspond to indices 0 and 1.
+//     printf("%s, %s\n", $row[0], $row[1]);
+//   }
+// }
 
-	if(isset($inviteCode) && is_string($inviteCode) && strlen($inviteCode) > 0){
-		foreach ($values as $i => $row) {
-			if (isset($row[10]) && $row[10] == $inviteCode){
-				// echo("Row: ". $i."\n");
-				$r = $i+1; //true row number
-				$writeRange = "Guestlist!L".$r.":N".$r;
-				// echo("What's the write range?  ". $writeRange);
-				// exit();
-				$values = [
-				    [$attending, $number_attending, date('Y-m-d H:i:s')] //Yes or No, size of party, time replied
-				];
-				$body = new Google_Service_Sheets_ValueRange([
-				  'values' => $values
-				]);
-				$params = [
-				  'valueInputOption' => "USER_ENTERED"
-				];
-				$result = $service->spreadsheets_values->update($spreadsheetId, $writeRange,
-				    $body, $params);
-				printf("%d cells updated.", $result->getUpdatedCells());
+$method = isset($argv[1]) && is_string($argv[1]) && strlen($argv[1]) > 0 ? $argv[1] : null;;
+//isset($_SERVER['REQUEST_METHOD']) && is_string($_SERVER['REQUEST_METHOD']) && in_array($_SERVER['REQUEST_METHOD'], array("GET", "POST")) ? $_SERVER['REQUEST_METHOD'] : "GET"
+	// $inviteCode = $_POST['code'];
+$inviteCode 	= (isset($argv[2]) && is_string($argv[2]) && strlen($argv[2]) > 0) ? $argv[2] : null;
+$attending 		= (isset($argv[3]) && is_string($argv[3]) && strlen($argv[3]) > 0) ? $argv[3] : null;
+$num_attending 	= (isset($argv[4]) && is_string($argv[4]) && strlen($argv[4]) > 0) ? $argv[4] : null;
+$notes 			= (isset($argv[5]) && is_string($argv[5]) && strlen($argv[5]) > 0) ? $argv[5] : null;
+
+function confirm($inviteCode, $attending, $num_attending, $notes){
+	global $spreadsheetId, $service, $values;
+	if(isset($attending) && is_string($attending) && isset($num_attending) && is_numeric($num_attending)){
+		if(isset($inviteCode) && is_string($inviteCode) && strlen($inviteCode) > 0){
+			// $service = $initSheet();
+			foreach ($values as $i => $row) {
+				if (isset($row[10]) && $row[10] == $inviteCode){
+					echo("Row: ". $i."\n");
+					$r = $i+1; //true row number
+					$writeRange = "Guestlist!L".$r.":O".$r;
+					echo("What's the write range?  ". $writeRange);
+					$vals = [
+					    [$attending, $num_attending, date('Y-m-d H:i:s'), "blah"] //Yes or No, size of party, time replied, notes
+					];
+					//(isset($notes) && is_string($notes) && strlen($notes)>0 ? $notes : null)
+					$body = new Google_Service_Sheets_ValueRange([
+					  'values' => $vals
+					]);
+					$params = [
+					  'valueInputOption' => "USER_ENTERED"
+					];
+					$result = $service->spreadsheets_values->update($spreadsheetId, $writeRange, $body, $params);
+					printf("%d cells updated.", $result->getUpdatedCells());
+				}
 			}
 		}
 	}
+	// $attending 			= "N";//(isset($_POST["attending"]) && is_bool($_POST["attending"]) ? $_POST["attending"] : null);
+	// $num_attending 		= 50;//(isset($_POST["size"]) && strlen(trim($_POST["size"])) > 0 ? trim($_POST["size"]) : null);
 }
-// exit();
-// echo($method);
-// echo("Invite code being searched is ".$inviteCode."\n");
-// exit();
-if($method === "GET"){
-	if (count($values) == 0) {
-	  	print "No data found.\n";
-	} else {
-		foreach ($values as $row) {
-			if (isset($row[10]) && $row[10] == $inviteCode){
-				// printf("%s has the code %s\n", $row[0], $inviteCode);
-				// echo($row[0]);
-				// echo("YAY");
 
-				$output = array(
-					"status" => "SUCCESS",
-					"record" => array(
-						"name" 	 	=> $row[0],
-						"size" 	 	=> $row[1],
-						"code" 	 	=> $row[10],
-						"attending" => isset($row[11]) ? $row[11] : "Awaiting Reply"
-					)
-				);
-				echo json_encode($output);
-				exit();
-			} 
+function lookup($invite_code){
+	global $values;
+	if(isset($invite_code) && is_string($invite_code) && strlen($invite_code) > 0){
+		// $service = initSheet();
+		if (count($values) == 0) {
+		  	print "No data found.\n";
+		} else {
+			foreach ($values as $row) {
+				if (isset($row[10]) && $row[10] == $invite_code){
+					// printf("%s has the code %s\n", $row[0], $inviteCode);
+					// print_r($row);
+					// echo("YAY");
+
+					$output = array(
+						"status" => "SUCCESS",
+						"record" => array(
+							"name" 	 	=> $row[0],
+							"size" 	 	=> $row[1],
+							"code" 	 	=> $row[10],
+							"attending" => isset($row[11]) ? $row[11] : "Awaiting Reply",
+							"notes"		=> isset($row[14]) ? $row[14] : null,
+						)
+					);
+					echo json_encode($output);
+					exit();
+				} 
+			}
+		  	$output = array(
+				"status"	=> "ERROR",
+				"message"	=> "No guest(s) found under that invite code. Please check spelling and try again."
+			);
+			echo json_encode($output);
 		}
-	  	$output = array(
-			"status"	=> "ERROR",
-			"message"	=> "No guest(s) found under that invite code. Please check spelling and try again."
-		);
-		echo json_encode($output);
 	}
 }
+
+
+
+if($method === "GET"){
+	lookup($inviteCode);
+}
+
+if($method === "POST"){
+	// echo("Invite code: ". $inviteCode."\n");
+	// echo("Attending: ". $attending)."\n";
+	// echo("Num attending: ". $num_attending)."\n";
+	// echo("Notes: ". $notes)."\n";
+	// echo("Calling: confirm(".$inviteCode.", ".$attending.", ".$num_attending.", ".$notes.")");
+	confirm($inviteCode, $attending, $num_attending, $notes);
+}
+
+// if($method === "POST"){
+// 	$attending 			= "N";//(isset($_POST["attending"]) && is_bool($_POST["attending"]) ? $_POST["attending"] : null);
+// 	$number_attending 	= 50;//(isset($_POST["size"]) && strlen(trim($_POST["size"])) > 0 ? trim($_POST["size"]) : null);
+
+// 	if(isset($inviteCode) && is_string($inviteCode) && strlen($inviteCode) > 0){
+// 		foreach ($values as $i => $row) {
+// 			if (isset($row[10]) && $row[10] == $inviteCode){
+// 				// echo("Row: ". $i."\n");
+// 				$r = $i+1; //true row number
+// 				$writeRange = "Guestlist!L".$r.":N".$r;
+// 				// echo("What's the write range?  ". $writeRange);
+// 				// exit();
+// 				$values = [
+// 				    [$attending, $number_attending, date('Y-m-d H:i:s')] //Yes or No, size of party, time replied
+// 				];
+// 				$body = new Google_Service_Sheets_ValueRange([
+// 				  'values' => $values
+// 				]);
+// 				$params = [
+// 				  'valueInputOption' => "USER_ENTERED"
+// 				];
+// 				$result = $service->spreadsheets_values->update($spreadsheetId, $writeRange,
+// 				    $body, $params);
+// 				printf("%d cells updated.", $result->getUpdatedCells());
+// 			}
+// 		}
+// 	}
+// }
 ?>
